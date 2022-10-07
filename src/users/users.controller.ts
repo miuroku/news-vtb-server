afd80a8403543936e126c3dc1df4e8e873bd5f11
Miuroku <thebestguy1101001@gmail.com>
2022-10-08 02:49:29 +0300

    simple protected route added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtAuthGuard } from 'src/auth/strategies/jwt/jwt-auth.guard';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  // @Get('/:id')
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  async getProfile(@Req() req: Request) {
+    return req.user;
+  }
+}
