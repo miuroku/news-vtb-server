d6b5a3266131c689b3d5ccfa8be433fb1d5b1a64
Miuroku <thebestguy1101001@gmail.com>
2022-10-09 01:30:40 +0300

    feat: port env added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   require('dotenv').config();
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
